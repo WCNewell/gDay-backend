a63c63f2e27645a17df92a5e880729a8efe31e87
Clark Newell <clarknewell@Clarks-MacBook-Pro.local>
2018-04-29 20:49:27 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+// const dbConnection = require("./connection.js");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"));
+
+app.get('/', (request, response) => response.send('Wassup Earth'))
+
+app.listen(PORT, () => console.log("Example app running"));

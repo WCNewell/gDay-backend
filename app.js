b5746fd1ffac9b88f4aab1d045d91630b4a3c4f7
Ryan <Rhackemer@gmail.com>
2018-04-30 14:02:08 -0600

    hello

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"))
 
 app.get('/', (request, response) => response.send('Wassup Earth'))
 
-app.listen(PORT, () => console.log("Example app running"))
+app.listen(PORT, () => console.log("Example app running")) //hello

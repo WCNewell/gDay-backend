78367c993d984dbb5390406829d2f9ce14d9c9f6
Ryan <Rhackemer@gmail.com>
2018-05-02 13:48:19 -0600

    integrated S# and Multer funcionality

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require("express")
 const morgan = require("morgan")
+const aws = require("aws-sdk")
+const multer = require("multer")
+const multerS3 = require("multer-s3")
+
 const dbConnection = require("./connection.js")
 const PORT = process.env.PORT || 3000
 const app = express()
+const router = express.Router()
+
+// This creates an authenticated S3 instance
+const s3 = new aws.S3({
+    apiVersion: "2006-03-01",
+    region: process.env.S3_BUCKET_REGION,
+    credentials: {
+        secretAccessKey: process.env.80K33PNAgW4wwJTTJ8fYeNS0SwTE+rdLB2A3bm9a,
+        accessKeyId: process.env.AKIAIGSNH5DMDYJLER2A
+    }
+})
+
+const upload = multer({
+    storage: multerS3({
+        s3, bucket: process.env.gday-m8,
+        key: (request, file, next) => { 
+            next(null, `files/${Date.now()}_${file.originalname}`);
+        }
+    })
+})
+
+router.post("/upload", upload.single("file"), (request, response) => {
+    response.json({data: request.file.location});
+});
+
+module.exports = router;
 
 app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"))
 

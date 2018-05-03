58d6dbca9350131f0406a3fcf0b3da91af28ad92
Ryan <Rhackemer@gmail.com>
2018-05-03 12:35:54 -0600

    S3, multer and aws obliterated

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express")
 const morgan = require("morgan")
-const aws = require("aws-sdk")
-const multer = require("multer")
-const multerS3 = require("multer-s3")
 const dbConnection = require("./connection.js")
 const cors = require("cors"
 )
@@ -12,24 +9,10 @@ const app = express()
 const router = express.Router()
 const PORT = process.env.PORT || 3000
 
-// This creates an authenticated S3 instance
-const s3 = new aws.S3({
-    apiVersion: "2006-03-01",
-    region: process.env.S3_BUCKET_REGION,
-    credentials: {
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-        accessKeyId: process.env.S3_ACCESS_KEY_ID
-    }
-})
 
-const upload = multer({
-    storage: multerS3({
-        s3, bucket: process.env.S3_BUCKET_NAME,
-        key: (request, file, next) => { 
-            next(null, `files/${Date.now()}_${file.originalname}`);
-        }
-    })
-})
+////UPLODA CODE GOES HERE
+
+
 
 router.post("/upload", upload.single("file"), (request, response) => {
     response.json({data: request.file.location});
